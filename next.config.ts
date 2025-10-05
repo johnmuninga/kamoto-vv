59f531aa070fc2b68dd2f27dc21e6ea51cddede1
johnmuninga <johnmuninga03@gmail.com>
2025-10-05 15:29:43 +0200

    dkjow

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@supabase/supabase-js']
-  },
-  images: {
-    domains: ['localhost']
-  }
+  /* config options here */
 };
 
 export default nextConfig;
